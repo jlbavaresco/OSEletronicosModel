158172656e98406e7c635ae87a34badbdbd01613
Jorge <Jorge@jorge-inspirion>
2019-08-23 18:43:19 -0300

    Ajustes no diagrama de classes e backup do banco

--- src/backup_completo.bat
+++ src/backup_completo.bat
@@ -2,6 +2,6 @@
 echo Efetuando backup completo do banco de dados PostgreSQL 10.5
 SET PGUSER=postgres
 SET PGPASSWORD=postgres
-C:\PostgreSQL\pg11\bin\pg_dump.exe --host localhost --port 5432 --username "postgres" --clean --format c --verbose --file backup_completo.backup "oseletronicos"
+C:\PostgreSQL\11\bin\pg_dump.exe --host localhost --port 5432 --username "postgres" --clean --format c --verbose --file backup_completo.backup "oseletronicos"
 
 
